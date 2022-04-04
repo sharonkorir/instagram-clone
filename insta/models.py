389e477fb526da32808b220595f94d64ad96712a
sharonkorir <sharon.korir@student.moringaschool.com>
2022-04-04 13:41:24 +0300

    create user registration page

--- insta/models.py
+++ insta/models.py
@@ -1,20 +1,18 @@
 from django.db import models
 from cloudinary.models import CloudinaryField
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Profile(models.Model):
+    '''
+    Profile model acts as blueprint for all profile instances
+    '''
+    bio = models.CharField(max_length =150)
+    profile_photo = CloudinaryField('image')
 
-class Post(models.Model):
-    '''
-    Post model acts as blueprint for all post instances
-    '''
-    name = models.CharField(max_length =60)
-    caption = models.TextField()
-    image = CloudinaryField('image')
-    #profile = models.ForeignKey(Profile, on_delete=models.DO_NOTHING)
-    #likes = models.ManyToMannyField(likes, on_delete = models.DO_NOTHING)
-    #comments = models.ManyToManyField(comments, on_delete=models.DO_NOTHING)
-    date_posted = models.DateTimeField(auto_now_add=True)
-    
+    def __str__(self):
+        return self.bio
+
 class likes(models.Model):
     count = models.IntegerField()
 
@@ -23,15 +21,27 @@ class likes(models.Model):
 
 class comments(models.Model):
     content = models.CharField(max_length =150)
-    #profile = models.ForeignKey(Profile, on_delete=models.DO_NOTHING)
+    # profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.content
+
+
+class Post(models.Model):
+    '''
+    Post model acts as blueprint for all post instances
+    '''
+    name = models.CharField(max_length =60)
+    caption = models.TextField()
+    image = CloudinaryField('image')
+    #profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    likes = models.ManyToManyField(likes)
+    comments = models.ManyToManyField(comments)
+    date_posted = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.name
+    
 
-class Profile(models.Model):
-    '''
-    Profile model acts as blueprint for all profile instances
-    '''
-    bio = models.CharField(max_length =150)
-    profile_photo = CloudinaryField('image')
+
     
--- insta/views.py
+++ insta/views.py
@@ -1,6 +1,12 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
+
+def register(request):
+    form = UserCreationForm()
+    return render(request, 'users/register.html', {'form': form})
+    
