c799dc11eb624bfd0400d016a3d323ad9f5f7384
sharonkorir <sharon.korir@student.moringaschool.com>
2022-04-04 11:50:32 +0300

    database: create profile model

--- insta/models.py
+++ insta/models.py
@@ -13,5 +13,25 @@ class Post(models.Model):
     #profile = models.ForeignKey(Profile, on_delete=models.DO_NOTHING)
     #likes = models.ManyToMannyField(likes, on_delete = models.DO_NOTHING)
     #comments = models.ManyToManyField(comments, on_delete=models.DO_NOTHING)
-    post_date = models.DateTimeField(auto_now_add=True)
+    date_posted = models.DateTimeField(auto_now_add=True)
+    
+class likes(models.Model):
+    count = models.IntegerField()
+
+    def __str__(self):
+        return self.count
+
+class comments(models.Model):
+    content = models.CharField(max_length =150)
+    #profile = models.ForeignKey(Profile, on_delete=models.DO_NOTHING)
+
+    def __str__(self):
+        return self.name
+
+class Profile(models.Model):
+    '''
+    Profile model acts as blueprint for all profile instances
+    '''
+    bio = models.CharField(max_length =150)
+    profile_photo = CloudinaryField('image')
     
