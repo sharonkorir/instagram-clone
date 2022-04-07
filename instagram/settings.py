c5150cd92075f2aaa0f22bd97d90c1b32823d385
sharonkorir <sharon.korir@student.moringaschool.com>
2022-04-07 11:54:24 +0300

    solve email STMP issue

--- instagram/settings.py
+++ instagram/settings.py
@@ -162,7 +162,7 @@ EMAIL_USE_TLS = config('EMAIL_USE_TLS')
 EMAIL_HOST = config('EMAIL_HOST')
 EMAIL_PORT = config('EMAIL_PORT')
 EMAIL_HOST_USER = config('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+EMAIL_HOST_PASSWORD = 'npszmxbaguxwziof'
 
 
 #redirect login from profile to homepage
